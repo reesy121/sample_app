d347848590e8ef7443b4cedb42a097275924094f
Dave Rees <reesy121@hotmail.co.uk>
2016-03-13 19:39:39 +0000

    Finnish user sign up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+  	get signup_path
+  	assert_no_difference 'User.count' do
+	  	post users_path, user: { name: "", 
+	  							 email: "foo@bar",
+	  							 password: "foo",
+  							 	 password_confirmation: "bar"}
+  	end
+  	assert_template 'users/new'
+  end
+
+   test "valid signup information" do
+  	get signup_path
+  	assert_difference 'User.count', 1 do
+	  	post_via_redirect users_path, user: { name: 		   		"Dave Rees", 
+					  						  email: 		 		"reesy121@hotmail.co.uk",
+					  						  password: 			 "ooohmelikey",
+				  							  password_confirmation: "ooohmelikey"}
+  	end
+  	assert_template 'users/show'
+  end
+
+
+end
